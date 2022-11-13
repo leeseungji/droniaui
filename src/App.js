44f6827beb65921df79b716c3791b14f0b9122e7
leeseungji <leesj0325@naver.com>
2022-11-13 17:16:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ResponsiveAppBar from './components/Nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pricing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div className="App">
+    PRICING ROUTE  
+    </div>
+  );
+}
+
+export default Pricing;
